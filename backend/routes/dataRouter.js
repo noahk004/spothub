af493b18805a7a934dc13ada1dfef55060f90701
Noah Kim <noahmk5002@gmail.com>
2024-07-09 21:17:23 -0700

    Add route to get all spots

--- backend/routes/dataRouter.js
+++ backend/routes/dataRouter.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const router = express.Router();
 
-router.get("/data", (req, res) => {
-  console.log(req.session.id)
-  res.status(201).send(
-    `Displaying session data: ${req.session.user.email}, ${req.session.user.password}`
-  );
+router.get("/data", async (req, res) => {
+  const client = new MongoClient(process.env.DB_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  const db = client.db("spothub");
+  const spotCollection = db.collection("spots");
+
+  const cursor = spotCollection.find({ school: "UC Irvine" });
+
+  const data = await cursor.toArray();
+
+  console.log(req.session.id);
+  res.status(201).json({ data });
 });
 
 module.exports = router;
