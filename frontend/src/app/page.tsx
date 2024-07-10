f1020298d0a09bddaa914893d80f38d97d7387a6
Noah Kim <noahmk5002@gmail.com>
2024-07-09 21:25:56 -0700

    Change landing page animation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className='container mx-auto'>
       <div className="h-screen flex items-center justify-center">
         <div className='flex-col justify-center'>
-          <h1 className="text-6xl font-bold mb-4 text-center">Welcome to <span className='drop-shadow-lg bg-gradient-to-r from-orange-500 to-red-800 inline-block text-transparent bg-clip-text'>SpotHub</span></h1>
+          <h1 className="text-6xl font-bold mb-4 text-center">Welcome to <span className='motion-safe:animate-bounce drop-shadow-lg bg-gradient-to-r from-orange-500 to-red-800 inline-block text-transparent bg-clip-text'>SpotHub</span></h1>
           <div className="text-xl text-center mb-4">Find out what people have to say about UC Irvine's most popular study spots!</div>
           <div className='flex justify-center '>
             <Button size='lg' asChild>
